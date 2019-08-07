cfb09f8189727d9872c94cb6cab5218da33ad528
Vanna Phong <contact@vanna.io>
2019-08-07 09:39:31 -0700

    Initial Commit

---
+++ CropCircle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CropCircle
+//
+//  Created by Vanna Phong on 07/08/2019.
+//  Copyright © 2019 vphong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
