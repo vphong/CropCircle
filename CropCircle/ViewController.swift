41f72cd042b9e67262257d736604eecc26546ae7
Vanna Phong <contact@vanna.io>
2019-08-07 10:50:34 -0700

    add gitignore, open imagepicker

--- CropCircle/ViewController.swift
+++ CropCircle/ViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    @IBAction func onGalleryButton(_ sender: Any) {
+        
+        // create image picker controller
+        let picker = UIImagePickerController()
+        picker.delegate = (self as UIImagePickerControllerDelegate & UINavigationControllerDelegate)
+        picker.sourceType = .photoLibrary
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
 }
 
